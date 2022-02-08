b7486a43be050ab1a5b677104be45b6b1af2f9f4
rubenphi <rubenphii@gmail.com>
2022-02-07 20:15:46 -0500

    se añaden dependencias de desarrollo

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-const { request } = require("express");
 const express = require("express");
 const app = express();
 app.use(express.json());
@@ -36,199 +35,7 @@ let respuestas = [
             created_at: "2022-01-27T21:49:59.000000Z",
             updated_at: "2022-01-27T21:49:59.000000Z",
         },
-    },
-    {
-        id: 70,
-        pregunta_id: 45,
-        grupo_id: 23,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "23-45",
-        created_at: "2022-01-28T13:22:42.480039Z",
-        updated_at: "2022-01-28T13:22:42.480039Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 23,
-            nombre: "grupo 5",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:50:14.000000Z",
-            updated_at: "2022-01-27T21:50:14.000000Z",
-        },
-    },
-    {
-        id: 71,
-        pregunta_id: 45,
-        grupo_id: 24,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "24-45",
-        created_at: "2022-01-28T13:22:42.685701Z",
-        updated_at: "2022-01-28T13:22:42.685701Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 24,
-            nombre: "grupo 6",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:50:20.000000Z",
-            updated_at: "2022-01-27T21:50:20.000000Z",
-        },
-    },
-    {
-        id: 72,
-        pregunta_id: 45,
-        grupo_id: 19,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "19-45",
-        created_at: "2022-01-28T13:22:53.853523Z",
-        updated_at: "2022-01-28T13:22:53.853523Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 19,
-            nombre: "grupo 1",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:49:35.000000Z",
-            updated_at: "2022-01-27T21:49:35.000000Z",
-        },
-    },
-    {
-        id: 73,
-        pregunta_id: 45,
-        grupo_id: 20,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "20-45",
-        created_at: "2022-01-28T13:22:57.033290Z",
-        updated_at: "2022-01-28T13:22:57.033290Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 20,
-            nombre: "grupo 2",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:49:49.000000Z",
-            updated_at: "2022-01-27T21:49:49.000000Z",
-        },
-    },
-    {
-        id: 74,
-        pregunta_id: 45,
-        grupo_id: 22,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "22-45",
-        created_at: "2022-01-28T13:23:07.049831Z",
-        updated_at: "2022-01-28T13:23:07.049831Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 22,
-            nombre: "grupo 4",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:50:06.000000Z",
-            updated_at: "2022-01-27T21:50:06.000000Z",
-        },
-    },
-    {
-        id: 75,
-        pregunta_id: 45,
-        grupo_id: 25,
-        opcion_id: 81,
-        cuestionario_id: 3,
-        puntaje: 5,
-        existe: 1,
-        grupoPregunta: "25-45",
-        created_at: "2022-01-28T13:23:10.596648Z",
-        updated_at: "2022-01-28T13:23:10.596648Z",
-        opcion: {
-            id: 81,
-            enunciado: "Un Verbo",
-            correcto: 1,
-            letra: "A",
-            existe: 1,
-            letraPregunta: "A-45",
-            correctoPregunta: "1-45",
-            pregunta_id: 45,
-            created_at: "2022-01-27T21:37:19.000000Z",
-            updated_at: "2022-01-27T21:37:19.000000Z",
-        },
-        grupo: {
-            id: 25,
-            nombre: "grupo 7",
-            existe: 1,
-            curso_id: 1,
-            created_at: "2022-01-27T21:50:28.000000Z",
-            updated_at: "2022-01-27T21:50:28.000000Z",
-        },
-    },
+    }
 ];
 /*
 const app = http.createServer((request, response) => {
@@ -245,10 +52,10 @@ app.get("/api/respuestas", (request, response) => {
 });
 
 app.post("/api/respuestas", (request, response) => {
-    const respuesta = request.body 
-    const ids = respuestas.map(respuesta => respuesta.id) 
-    const maxId = Math.max(...ids)
-    
+    const respuesta = request.body;
+    const ids = respuestas.map((respuesta) => respuesta.id);
+    const maxId = Math.max(...ids);
+
     const newRespuesta = {
         id: maxId + 1,
         pregunta_id: respuesta.pregunta_id,
@@ -257,12 +64,11 @@ app.post("/api/respuestas", (request, response) => {
         cuestionario_id: respuesta.cuestionario_id,
         puntaje: respuesta.puntaje,
         existe: respuesta.existe || 1,
-        grupoPregunta: respuesta.grupo_id + "-" + respuesta.pregunta_id
+        grupoPregunta: respuesta.grupo_id + "-" + respuesta.pregunta_id,
+    };
 
-    }
-
-    respuestas = [... respuestas, newRespuesta  ]
-    response.json(newRespuesta)
+    respuestas = [...respuestas, newRespuesta];
+    response.json(newRespuesta);
 });
 
 app.get("/api/respuestas/:id", (request, response) => {
@@ -278,7 +84,7 @@ app.get("/api/respuestas/:id", (request, response) => {
 app.delete("/api/respuestas/:id", (request, response) => {
     const id = Number(request.params.id);
     respuestas = respuestas.filter((respuesta) => respuesta.id != id);
-    response.status(204).end()
+    response.status(204).end();
 });
 
 const PORT = 3001;

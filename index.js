d3c69706057cbcfbc6570c5caabb1cd39854d477
rubenphi <rubenphii@gmail.com>
2022-02-08 06:58:22 -0500

    se añaden mas dependencias

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require("express");
+const cors = require("cors");
 const app = express();
+app.use(cors());
 app.use(express.json());
+
+app.use((request, response, next) => {
+    console.log(request.method);
+    console.log(request.path);
+    console.log(request.body);
+    console.log("------------");
+    next();
+});
 
 // const http = require('http')
 let respuestas = [
@@ -87,6 +97,13 @@ app.delete("/api/respuestas/:id", (request, response) => {
     response.status(204).end();
 });
 
+
+app.use((request, response) => {
+    const ruta = request.path;
+    response.status(404).json({
+        "error": "la ruta " + ruta +  " no ha sido encontrada",
+    });
+});
 const PORT = 3001;
 app.listen(PORT, () => {
     console.log(`Servidor corriendo en el puerto ${PORT}`);
